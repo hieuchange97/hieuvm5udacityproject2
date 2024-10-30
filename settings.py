c495f415a49fb261d40413429ce0a46b476dae5a
Minh Hiếu <44992480+hieuchange97@users.noreply.github.com>
2024-10-30 22:39:29 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,4 +16,4 @@ API_VERSION = 'v1'
 #API_URL = " https://neighborlyapi.azurewebsites.net/api/"
 
 # for local host if Azure functions served locally
-API_URL = "https://hieuvm5functionapp.azurewebsites.net"
+API_URL = "https://hieuvm5functionapps.azurewebsites.net"
